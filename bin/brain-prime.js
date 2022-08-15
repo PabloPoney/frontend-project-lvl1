87f87e396d3fd7e7823fe0f85bf8b99900c4cc72
HAL 9000 <pablohoney720@gmail.com>
2022-08-15 20:59:50 +0400

    maked prime-game

---
+++ bin/brain-prime.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import _ from 'lodash';
+import readlineSync from 'readline-sync';
+import { acquaintance, gameLogic, randomNumber } from '../src/index.js';
+
+const isItPrime = (num) => {
+  const halfNum = Math.floor(num / 2);
+  for (let i = halfNum; i > 1; i -= 1) {
+    if (num % i === 0) return 'no';
+  }
+  return 'yes';
+};
+
+const discription = 'Answer "yes" if the number is prime, otherwise answer "no".';
+
+const gameStep = () => {
+  const num = randomNumber(5);
+  const prime = isItPrime(num);
+  console.log(`Question: ${num}`);
+  const answer = readlineSync.question('Your asnswer: ');
+  if (answer.toLowerCase() !== prime) {
+    console.log(`${_.capitalize(answer)} is wrong answer ;(. Correct answer was ${prime}.`);
+    return false;
+  }
+  console.log('Correct');
+  return true;
+};
+
+const name = acquaintance();
+gameLogic(name, discription, gameStep);
